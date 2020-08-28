548446d22d370b662c4035be1c43aec8c702c907
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-08-28 09:47:25 -0300

    test e2e login

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, by, element } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,9 +8,41 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display Iniciar sessão no Steam', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('steam app is running!');
+    expect(page.getTitleText()).toEqual('Iniciar sessão no Steam');
+  });
+
+  it('Login Steam', () => {
+    element(by.css("#user")).sendKeys('lucasferreiralimax@gmail.com');
+    element(by.css("#pass")).sendKeys('steam');
+    browser.sleep(2000)
+    element(by.css("#session")).click();
+    browser.switchTo().alert().accept();
+    browser.sleep(2000)
+  });
+
+  it('Login Clean', () => {
+    page.navigateTo();
+    element(by.css("#user")).sendKeys('lucasferreiralimax@gmail.com');
+    element(by.css("#pass")).sendKeys('steam');
+    browser.sleep(2000)
+    element(by.css("#cancel")).click();
+    browser.sleep(2000)
+  });
+
+  it('should login close and new', () => {
+    element(by.css("#close")).click();
+    browser.sleep(2000)
+    element(by.css(".btn.new")).click();
+    browser.sleep(2000)
+  });
+
+  it('should login minimize handler', () => {
+    element(by.css("#minimize")).click();
+    browser.sleep(2000)
+    element(by.css("#minimize")).click();
+    browser.sleep(2000)
   });
 
   afterEach(async () => {
