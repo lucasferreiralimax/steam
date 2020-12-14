dff18587efd9ca932bc19272444a463e345d1ab2
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-14 05:44:56 -0300

    clean empty styles

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.styl']
+  templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
 
